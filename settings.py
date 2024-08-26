2b97e9887e86e950adc0a33c90fc8d13e22e3775
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-08-26 23:45:15 +0300

    - added ship side movements through left/right arrow key press events - added restrictions in 'ship' module for the ship not to go beyond the screen borders - moved ship movement speed from 'ship' module to 'settings' module

--- settings.py
+++ settings.py
@@ -7,3 +7,6 @@ class Settings:
         self.screen_width: int = 1200
         self.screen_height: int = 800
         self.bg_color: tuple[int, int, int] = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed: float = 1.5
--- ship.py
+++ ship.py
@@ -7,14 +7,36 @@ class Ship:
     def __init__(self, ai_game):
         """Initiate the ship and set its starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load ship image and get its rect
-        self.image = pygame.image.load('images/ship.bmp')
-        self.rect = self.image.get_rect()
+        self.image: pygame.Surface = pygame.image.load('images/ship.bmp')
+        self.rect: pygame.Rect = self.image.get_rect()
 
         # Create every new ship at the screen bottom, in the center
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Save a float value for the ship horizontal position
+        self.x: float = float(self.rect.x)
+
+        # Movement indicators
+        self.moving_right: bool = False
+        self.moving_left: bool = False
+
+    def update(self):
+        """
+        Update the ship's current location based on
+        the movement indicators
+        """
+        # Update ship.x value, not rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update the object's rect from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location"""

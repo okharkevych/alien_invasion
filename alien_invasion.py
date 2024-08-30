6c4aac1609ffe255851233129800eb1fdef019f7
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-08-30 17:16:32 +0300

    implemented starting game session by pressing 'p' key as an alternative to clicking the 'Play' button

--- alien_invasion.py
+++ alien_invasion.py
@@ -74,20 +74,37 @@ class AlienInvasion:
         """Start a new game once user presses the 'Play' button"""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
-            # Reset game statistics
-            self.stats.reset_stats()
-            self.stats.game_active = True
+            # # Reset game statistics
+            # self.stats.reset_stats()
+            # self.stats.game_active = True
+            #
+            # # Remove excess aliens and bullets
+            # self.aliens.empty()
+            # self.bullets.empty()
+            #
+            # # Create a new fleet and center the ship
+            # self._create_fleet()
+            # self.ship.center_ship()
+            #
+            # # Hide mouse cursor
+            # pygame.mouse.set_visible(False)
+            self._start_game()
 
-            # Remove excess aliens and bullets
-            self.aliens.empty()
-            self.bullets.empty()
+    def _start_game(self):
+        # Reset game statistics
+        self.stats.reset_stats()
+        self.stats.game_active = True
 
-            # Create a new fleet and center the ship
-            self._create_fleet()
-            self.ship.center_ship()
+        # Remove excess aliens and bullets
+        self.aliens.empty()
+        self.bullets.empty()
 
-            # Hide mouse cursor
-            pygame.mouse.set_visible(False)
+        # Create a new fleet and center the ship
+        self._create_fleet()
+        self.ship.center_ship()
+
+        # Hide mouse cursor
+        pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self, event: pygame.KEYDOWN):
         """React to key presses"""
@@ -97,6 +114,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_p and not self.stats.game_active:
+            self._start_game()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
 

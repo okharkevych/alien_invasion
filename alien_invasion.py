88c64a10d314a0c03aa981971c4e86f2304db68d
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2024-09-01 22:03:19 +0300

    ships (a.k.a. ship lives) left are displayed in the top left corner of the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -86,6 +86,7 @@ class AlienInvasion:
         self.stats.game_active = True
         self.sb.prep_score()
         self.sb.prep_level()
+        self.sb.prep_ships()
 
         # Remove excess aliens and bullets
         self.aliens.empty()
@@ -182,6 +183,7 @@ class AlienInvasion:
         if self.stats.ships_left > 0:
             # Reduce ships_left
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Remove excess aliens and bullets
             self.aliens.empty()
